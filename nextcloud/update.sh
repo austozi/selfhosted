f28bd3445d0f6f49edb27d023e206632c970445f
austozi <89324189+austozi@users.noreply.github.com>
2022-05-22 22:44:43 +0100

    Create update.sh

---
+++ nextcloud/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+# Save this script as /path/to/nextcloud/cron/update.sh.
+# /path/to/nextcloud/ is where docker-compose.yml is.
+# Make it executable: chmod +x /path/to/nextcloud/cron/update.sh.
+# Invoke this script as a nightly cron job to auto-update Nextcloud.
+
+projectPath="/path/to/nextcloud/"
+logFile="$projectPath/cron/update.log"
+
+# Update Nextcloud core application
+docker exec -u abc -it nextcloud php /config/www/nextcloud/updater/updater.phar --no-interaction
+
+# Update database
+docker exec -u abc -it nextcloud /config/www/nextcloud/occ db:add-missing-indices --no-interaction
+
+# Update Nextcloud apps
+docker exec -u abc -it nextcloud /config/www/nextcloud/occ app:update --all --no-interaction
