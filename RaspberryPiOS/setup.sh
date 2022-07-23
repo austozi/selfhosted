c72d354150fa855457c3dd5634924a9646e84a4b
austozi <89324189+austozi@users.noreply.github.com>
2022-07-23 15:33:55 +0100

    Update setup.sh

--- RaspberryPiOS/setup.sh
+++ RaspberryPiOS/setup.sh
@@ -2,6 +2,7 @@
 # Execute this script as superuser
 apt-get update
 apt-get -yy dist-upgrade
-apt-get -yy install ansible-pull
+apt-get -yy install ansible-pull git
 apt-get -yy autoremove --purge
 apt-get -yy autoclean
+curl -sSL https://get.docker.com | bash
