72ac16124d3fd6dcd7cc08efb66738293c6057c2
austozi <89324189+austozi@users.noreply.github.com>
2022-07-23 15:48:06 +0100

    Update setup.sh

--- RaspberryPiOS/setup.sh
+++ RaspberryPiOS/setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -e
 # Execute this script as superuser
+
+##########################
+# Customise the hostname #
+##########################
 read -p "Hostname: " hostname
 hostnamectl set-hostname "$hostname" --static
+
+#########################################
+# Update system and enable ansible-pull #
+#########################################
 apt-get update
 apt-get -yy dist-upgrade
 apt-get -yy install ansible git
 apt-get -yy autoremove --purge
 apt-get -yy autoclean
+
+################################################
+# Install docker and grant default user access #
+################################################
 docker --version &> /dev/null || curl -sSL https://get.docker.com | bash
 adduser --uid 1000 docker
