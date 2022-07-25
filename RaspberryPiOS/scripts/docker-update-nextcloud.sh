c3615ec6d04c2de9ca68e6b4ec11ffc450c942c4
austozi <89324189+austozi@users.noreply.github.com>
2022-07-25 17:01:08 +0100

    Create docker-update-nextcloud.sh

---
+++ RaspberryPiOS/scripts/docker-update-nextcloud.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+################################################################################
+# docker-update-nextcloud.sh
+# ==========================
+# Copyright 2022 austozi.
+# Shell script to update the linuxserver/nextcloud container.
+# Unlike the official container image, this requires that the container be 
+# updated from within the running container itself.
+# This script will update the nextcloud application, update database indices,
+# and update any Nextcloud apps (add-ons) installed.
+# Execute this script on the docker host to run the update.
+# Or, schedule a cronjob to automate it.
+# ##############################################################################
+
+CONTAINER_NAME=nextcloud
+
+docker exec -u abc -it "$CONTAINER_NAME" php /config/www/nextcloud/updater/updater.phar --no-interaction
+docker exec -u abc -it "$CONTAINER_NAME" /config/www/nextcloud/occ db:add-missing-indices --no-interaction
+docker exec -u abc -it "$CONTAINER_NAME" /config/www/nextcloud/occ app:update --all --no-interaction
