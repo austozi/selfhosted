fbbfd240dd21ae1be14903f2f322ce4ebdd4da38
austozi <89324189+austozi@users.noreply.github.com>
2022-07-28 21:29:54 +0100

    Update signal.sh

--- fdroidserver/update-scripts/signal.sh
+++ fdroidserver/update-scripts/signal.sh
@@ -1,6 +1,21 @@
 #!/bin/bash -e
+
+# Install dependencies first.
 apk add --update --no-cache curl jq &> /dev/null
-version=`curl -sSL 'https://updates.signal.org/android/latest.json' | jq -r .versionName` &> /dev/null
-url=`curl -sSL 'https://updates.signal.org/android/latest.json' | jq -r .url` &> /dev/null
-echo "Downloading $url..."
-curl -sSL -o "/config/fdroid/repo/org.thoughtcrime.securesms_$version.apk" "$url" &> /dev/null
+
+APPNAME=Signal
+echo "Checking updates for $APPNAME..."
+
+version="`curl -sSL 'https://updates.signal.org/android/latest.json' | jq -r .versionName`" &> /dev/null
+APKNAME="org.thoughtcrime.securesms_$version.apk"
+
+# Download only if the file doesn't already exist, i.e. a new version is available.
+FILE="/config/fdroid/repo/$APKNAME"
+if [ ! -f "$FILE" ]; then
+	echo "New version available: $version"
+	url="`curl -sSL 'https://updates.signal.org/android/latest.json' | jq -r .url`" &> /dev/null
+	echo "Downloading from $url..."	
+	curl -sSL -o "$FILE" "$url" &> /dev/null
+else
+	echo "$APPNAME already up to date."
+fi
